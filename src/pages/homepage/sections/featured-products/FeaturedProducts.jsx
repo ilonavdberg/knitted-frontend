e8b99a3d3f1672d2e0ac312cf6f9e8067c8c5690
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2025-01-15 11:37:03 +0100

    implemented fetching products from the backend in the product catalog page

--- src/pages/homepage/sections/featured-products/FeaturedProducts.jsx
+++ src/pages/homepage/sections/featured-products/FeaturedProducts.jsx
@@ -25,16 +25,15 @@ function FeaturedProducts() {
         })
     }, [])
 
-    //This useEffect is added for testing purposes
     useEffect(() => {
         console.log(products);
-    }, [products]); // Runs only when products changes
+    }, [products]);
 
     return (
         <section className="featured-products">
             <h2 className="featured-products__title">Featured products</h2>
-            <div className="featured-products__products">
-                {products.map((product) => {
+            <ul className="featured-products__products">
+                {products.map(product => {
                     return <ProductCard
                         key={product.id}
                         title={product.title}
@@ -45,41 +44,7 @@ function FeaturedProducts() {
                         variant="featured"
                     />;
                 })}
-            </div>
-            {/*<div className="featured-products__products">*/}
-            {/*    <ProductCard*/}
-            {/*        title={products[0]?.title}*/}
-            {/*        price={products[0]?.price}*/}
-            {/*        photo={products[0]?.itemPhoto?.base64Image}*/}
-            {/*        shopName={products[0]?.shopName}*/}
-            {/*        shopImage={products[0]?.shopPicture?.base64Image}*/}
-            {/*        variant="featured"*/}
-            {/*    />*/}
-            {/*    <ProductCard*/}
-            {/*        title={products[1]?.title}*/}
-            {/*        price={products[1]?.price}*/}
-            {/*        photo={products[1]?.itemPhoto?.base64Image}*/}
-            {/*        shopName={products[1]?.shopName}*/}
-            {/*        shopImage={products[1]?.shopPicture?.base64Image}*/}
-            {/*        variant="featured"*/}
-            {/*    />*/}
-            {/*    <ProductCard*/}
-            {/*        title={products[2]?.title}*/}
-            {/*        price={products[2]?.price}*/}
-            {/*        photo={products[2]?.itemPhoto?.base64Image}*/}
-            {/*        shopName={products[2]?.shopName}*/}
-            {/*        shopImage={products[2]?.shopPicture?.base64Image}*/}
-            {/*        variant="featured"*/}
-            {/*    />*/}
-            {/*    <ProductCard*/}
-            {/*        title={products[3]?.title}*/}
-            {/*        price={products[3]?.price}*/}
-            {/*        photo={products[3]?.itemPhoto?.base64Image}*/}
-            {/*        shopName={products[3]?.shopName}*/}
-            {/*        shopImage={products[3]?.shopPicture?.base64Image}*/}
-            {/*        variant="featured"*/}
-            {/*    />*/}
-            {/*</div>*/}
+            </ul>
         </section>
     );
 }
--- src/pages/product-catalog-page/ProductCatalogPage.jsx
+++ src/pages/product-catalog-page/ProductCatalogPage.jsx
@@ -2,17 +2,52 @@ import './ProductCatalogPage.css'
 
 import PageLayout from "../../components/pagelayout/PageLayout.jsx";
 import Menu from "@/pages/product-catalog-page/sections/menu/Menu.jsx";
-import Products from "@/pages/product-catalog-page/sections/products/Products.jsx";
 import PageSelector from "@/components/pageselector/PageSelector.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import ProductCard from "@/components/productcard/ProductCard.jsx";
 
 function ProductCatalogPage() {
     // here the state data and functions will be stored
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function fetchProducts() {
+            try {
+                return await axios.get("http://localhost:8080/v1/items");
+                // setProducts(response.data.content);
+            } catch(e) {
+                console.error(e);
+            }
+        }
+
+        fetchProducts().then((response) => {
+            console.log(response);
+            setProducts(response.data.content);
+        })
+    }, [])
+
+    useEffect(() => {
+        console.log(products);
+    }, [products]);
 
     return (
         <PageLayout>
             <section>
                 <Menu />
-                <Products />
+                {/*<Products />*/}
+                <ul className="products">
+                    {products.map(product => {
+                        return <ProductCard
+                            key={product.id}
+                            title={product.title}
+                            price={product.price}
+                            photo={product.itemPhoto.base64Image}
+                            shopName={product.shopName}
+                            shopImage={product.shopPicture.base64Image}
+                        />
+                    })}
+                </ul>
                 <PageSelector />
             </section>
         </PageLayout>
