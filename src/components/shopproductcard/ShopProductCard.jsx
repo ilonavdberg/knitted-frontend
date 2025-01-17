ac2559b57b4104ff72eb9a175b0f12c3c5dc8295
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2025-01-17 13:26:43 +0100

    fetched product data for shop page

--- src/components/shopproductcard/ShopProductCard.jsx
+++ src/components/shopproductcard/ShopProductCard.jsx
@@ -2,19 +2,19 @@ import './ShopProductCard.css';
 import {Link} from "react-router-dom";
 
 
-function ShopProductCard({ status }) {
+function ShopProductCard({ id, title, price, status, photo }) {
     return (
-        <Link to="/product">
+        <Link to={`/product/${id}`}>
             <article className={"shop-product-card"}>
                 <section className="shop-product-card__product">
                     <div className={`shop-product-card__label label--${status}`}>
                         <p>{status}</p>
                     </div>
                     <div className="shop-product-card__product-photo">
-                        <img src="src/assets/images/product_photo.png" alt="product photo"/>
+                        <img src={`data:image/png;base64,${photo}`} alt="product photo"/>
                     </div>
-                    <h3 className="shop-product-card__product-title">Longest allowed Product title</h3>
-                    <p className="shop-product-card__product-price">€125,00</p>
+                    <h3 className="shop-product-card__product-title">{title}</h3>
+                    <p className="shop-product-card__product-price">{`€${price}`}</p>
                 </section>
             </article>
         </Link>
