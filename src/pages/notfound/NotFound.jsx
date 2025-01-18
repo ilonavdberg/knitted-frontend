e6eb64e74d03ffbe1a803874b08e1fd0b89f3c48
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2025-01-18 18:10:40 +0100

    added content and styles to not found page

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,10 +1,17 @@
 import './NotFound.css'
 import PageLayout from "../../components/pagelayout/PageLayout.jsx";
+import {Link} from "react-router-dom";
 
 function NotFound() {
     return (
         <PageLayout>
-            <p>Not Found Page</p>
+            <div className="not-found">
+                <h2 className="not-found__error-code">404</h2>
+                <h1 className="not-found__title">Oops! Nothing was found</h1>
+                <p className="not-found__text">The page you are looking for might have been removed, had its name changed or is temporarily
+                    unavailable.</p>
+                <p className="not-found__text">Return to <Link to="/" className="not-found__link-homepage" >Homepage</Link></p>
+            </div>
         </PageLayout>
     );
 }
