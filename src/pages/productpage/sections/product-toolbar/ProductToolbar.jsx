8ce007f0168f3c45be233460e567ffdb28c687d6
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2025-01-21 13:14:35 +0100

    rendered error message when publication validation was unsuccessful

--- src/pages/productpage/sections/product-toolbar/ProductToolbar.jsx
+++ src/pages/productpage/sections/product-toolbar/ProductToolbar.jsx
@@ -4,10 +4,12 @@ import {Trash} from "@phosphor-icons/react";
 import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 import {BASE_URL} from "@/utils/UrlBuilder.js";
+import {useState} from "react";
 
 function ProductToolbar({ product, refresh }) {
     const { id } = useParams();
     const navigate = useNavigate();
+    const [error, setError] = useState("");
 
     async function handleArchiveProduct() {
         try {
@@ -28,6 +30,7 @@ function ProductToolbar({ product, refresh }) {
                 navigate(`/shop/${response.data.shop.id}`);
             }
         } catch(e) {
+            setError(e.response.data)
             console.error(e);
         }
     }
@@ -45,28 +48,36 @@ function ProductToolbar({ product, refresh }) {
         <header className="product-toolbar">
             <h1 className="product-toolbar__title">Product Preview</h1>
             {product.status === "draft" && (
-                <div className="product-toolbar__actions">
-                    <Button
-                        onClick={handlePublishProduct}
-                        skin="primary"
-                    >
-                        Publish
-                    </Button>
-                    <Button
-                        onClick={() => {
-                            navigate(`/product/${id}/edit`)
-                        }}
-                        skin="secondary"
-                    >
-                        Edit
-                    </Button>
-                    <Button
-                        onClick={handleArchiveProduct}
-                        skin="transparent"
-                    >
-                        <Trash size={32}/>
-                    </Button>
-                </div>
+                <>
+                    <div className="product-toolbar__actions">
+                        <Button
+                            onClick={handlePublishProduct}
+                            skin="primary"
+                        >
+                            Publish
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                navigate(`/product/${id}/edit`)
+                            }}
+                            skin="secondary"
+                        >
+                            Edit
+                        </Button>
+                        <Button
+                            onClick={handleArchiveProduct}
+                            skin="transparent"
+                        >
+                            <Trash size={32}/>
+                        </Button>
+                    </div>
+                    <div>
+                        {error && (
+                            <p className="error-message">{error}</p>
+                        )}
+                    </div>
+                </>
+
             )}
             {product.status === "published" && (
                 <div className="product-toolbar__actions">
